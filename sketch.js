a3e464bc00d33f9ba15facc3dcfcf6374c6b7f2b
sschloss04 <130077700+sschloss04@users.noreply.github.com>
2023-04-19 14:15:32 -0400

    website

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Color palette:
+// rgb(207, 240, 158)
+// rgb(168, 219, 168) <-- background
+// rgb(121, 189, 154) <-- main color?
+// rgb(59, 134, 134)
+// rgb(11, 72, 107) <--accent
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  clear();
+  background(168,219,168);
+  noLoop();
+}
